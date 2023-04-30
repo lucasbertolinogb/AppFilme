7dd85c3cef5df57b32835e54e54716a95cd72eb9
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-04-30 15:08:14 -0300

    Create index.js

---
+++ Projeto/index.js
@@ -0,0 +1,23 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import styles from '../components/styles';
+  
+  const baseUrl ="https://api.otaviolube.com";
+
+export default function CardFilme({ filme }){
+
+    return (
+         <View style={styles.box}>
+           
+            <Image style={styles.imagem}source={{ uri: baseUrl + filme.poster.data.attributes.url }}/>
+           
+            <Text style={styles.titulo}>{filme.titulo} </Text>
+
+            <Text style={styles.sinopse}>{filme.sinopse}</Text>
+           
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttomLetra}>Comprar</Text>
+            </TouchableOpacity>
+          
+          </View>
+    )
+  }
