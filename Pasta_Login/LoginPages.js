7c9f0cd187e5ea3c60ba565415f28c313c286197
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-05-12 10:47:39 -0300

    Create LoginPages.js

---
+++ Pasta_Login/LoginPages.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+
+
+
+export default function LoginPages(){
+    return(
+        <View>
+            <Text> Teste login</Text>
+            <Link></Link>
+        </View>
+    )
+
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
