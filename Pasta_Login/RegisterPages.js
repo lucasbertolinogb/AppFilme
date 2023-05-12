7bea0e34057abbe9f38d4d186a9b3962d630a05c
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-05-12 10:48:00 -0300

    Create RegisterPages.js

---
+++ Pasta_Login/RegisterPages.js
@@ -0,0 +1,9 @@
+import { Text, StyleSheet, View } from "react-native-web";
+
+export default function RegisterPages(){
+    return(
+        <View style={StyleSheet.container}>
+            <Text>Pagina Resgistro</Text>
+        </View>
+    )
+}
