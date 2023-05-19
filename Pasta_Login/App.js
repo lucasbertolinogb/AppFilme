566e434f698499300df693e0ba2bd8dbe514b3c5
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-05-19 09:58:58 -0300

    Update App.js

--- Pasta_Login/App.js
+++ Pasta_Login/App.js
@@ -1,16 +1,16 @@
-import RegisterPages from "./src/pages/RegisterPages";
-import LoginPages from "./src/pages/LoginPages";
+import RegisterPages from "./src/components/RegisterPages";
+import LoginPages from "./src/components/LoginPages";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
+import 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Stack.Navigator>
-    <Stack.Screen name="Registro" component={RegisterPages} />
-  </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Registro" component={RegisterPages} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
