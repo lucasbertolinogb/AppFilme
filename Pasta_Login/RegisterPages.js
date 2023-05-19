c4b5159a6788e0dbc7f9daed20a72226575a7b84
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-05-19 09:59:29 -0300

    Update RegisterPages.js

--- Pasta_Login/RegisterPages.js
+++ Pasta_Login/RegisterPages.js
@@ -1,9 +1,18 @@
 import { Text, StyleSheet, View } from "react-native-web";
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function RegisterPages(){
+    const navigation = useNavigation();
+
+    const goToProfile = () => {
+        navigation.navigate('Profile');
+      };
+    
     return(
-        <View style={StyleSheet.container}>
-            <Text>Pagina Resgistro</Text>
-        </View>
-    )
+        <Button title="Go to Profile" onPress={goToProfile} />
+        
+    
+    
+    );
 }
