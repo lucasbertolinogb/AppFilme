2939526b7431a458504c8f47101e45fe0619442e
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-06-23 09:45:23 -0300

    Create index.js

---
+++ Projeto_2v/src/routes/index.js
@@ -0,0 +1,24 @@
+import AppRoutes from "./AppRoutes";
+import AuthRoutes from "./Authroutes";
+import { useAuth } from "../contexts/AuthContext";
+import { ActivityIndicator, View } from "react-native";
+
+export default function Routes(){
+
+    const { logado, loading } = useAuth();
+
+    if(loading){
+        return (
+            <View style={{flex: 1 ,
+             alignItems: 'center',
+             justifyContent: 'center'}}>
+                <ActivityIndicator size={'large'}/>
+            </View>
+        )
+    }
+
+    return (
+        logado == true ? <AppRoutes /> 
+        : <AuthRoutes />
+    );
+}
